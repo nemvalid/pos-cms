d89dc6e894584d6c32fa1fd042a6cc83d8220d97
Peter Almasi <Peter_Almasi@outlook.com>
2022-05-29 17:21:47 +0200

    admin module wip; content manager updates

--- modules/content-manager/src/components/Layout.tsx
+++ modules/content-manager/src/components/Layout.tsx
@@ -79,40 +79,6 @@ export default function Layout() {
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px'
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' })
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              pOS CMS
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
         <Drawer variant="permanent" open={open}>
           <Toolbar
             sx={{
